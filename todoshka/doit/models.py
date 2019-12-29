98ecd67d1441c3250444504bd2c0085097adda0c
dima <amid.vonavi@gmail.com>
2019-12-30 00:34:06 +0200

    view updated for verbose

--- todoshka/doit/models.py
+++ todoshka/doit/models.py
@@ -7,9 +7,13 @@ class Lists(models.Model):
     list_name = models.CharField(max_length=500)
     pub_date = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.list_name
+
 
 class Tasks(models.Model):
     to_list = models.ForeignKey(Lists, related_name='to_list', on_delete=models.CASCADE)
     task_name = models.CharField(max_length=5000)
     end_date = models.DateTimeField(null=True)
     is_done = models.BooleanField(default=False)
+
--- todoshka/doit/views.py
+++ todoshka/doit/views.py
@@ -4,4 +4,13 @@ from .models import Lists, Tasks
 
 
 def index(request):
-    return HttpResponse("Hi!")
+    lists = Lists.objects.all()
+    lists_names = list()
+
+    for l in lists:
+        lists_names.append(l.list_name)
+
+    response_html = '<br>'.join(lists_names)
+
+    return HttpResponse(response_html)
+
