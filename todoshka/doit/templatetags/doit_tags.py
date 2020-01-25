9a587ecfb8daca18397de8794ef8da8916faa81f
dima <amid.vonavi@gmail.com>
2020-01-25 22:55:56 +0200

    added humanize and text truncator

--- todoshka/doit/templatetags/doit_tags.py
+++ todoshka/doit/templatetags/doit_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from django.utils.timezone import utc
+from django.utils.text import Truncator
 from datetime import datetime
 
 
@@ -9,3 +10,8 @@ register = template.Library()
 @register.simple_tag
 def time_diff(start_date):
     return (datetime.utcnow().replace(tzinfo=utc) - start_date).days
+
+
+@register.simple_tag
+def text_truncator(txt, word_num):
+    return Truncator(txt).words(word_num)
