3c63acdac4ff53974da93fb2801c7589257a942c
dima <amid.vonavi@gmail.com>
2020-01-27 22:57:24 +0200

    admin: added custom actions and non changeble field

--- todoshka/doit/admin.py
+++ todoshka/doit/admin.py
@@ -2,5 +2,33 @@ from django.contrib import admin
 
 from .models import List, Task
 
-admin.site.register(List)
-admin.site.register(Task)
+
+# def task_custom_action(modeladmin, request, queryset):
+#     for task in queryset:
+#         task.task_name = task.task_name + "__some text from custom actions"
+#         task.save()
+#     #queryset.update(task_name='p')
+
+
+#task_custom_action.short_description = "Add some static text to task_name"
+
+
+class ListAdmin(admin.ModelAdmin):
+    readonly_fields = ('pub_date',)
+
+
+class TaskAdmin(admin.ModelAdmin):
+    readonly_fields = ('pub_date',)
+    actions = ('task_custom_action', )
+
+    def task_custom_action(self, request, queryset):
+        for task in queryset:
+            task.task_name = task.task_name + "__some text from custom actions"
+            task.save()
+        # queryset.update(task_name='p')
+
+    task_custom_action.short_description = "Add some static text to task_name"
+
+
+admin.site.register(List, ListAdmin)
+admin.site.register(Task, TaskAdmin)
