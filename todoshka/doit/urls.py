b80f732413f2b84abb455cf21ef779daeb96cbbc
dima <amid.vonavi@gmail.com>
2020-02-06 22:29:11 +0200

    slug in url instead of model id

--- todoshka/doit/urls.py
+++ todoshka/doit/urls.py
@@ -6,10 +6,10 @@ app_name = 'doit'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('l/<int:list_id>/', views.list, name='list'),
-    path('l/<int:list_id>/u/', views.list_update, name='list_update'),
-    path('t/<int:task_id>/', views.task, name='task'),
-    path('t/<int:task_id>/u', views.task_update, name='task_update'),
+    path('l/<slug:list_id>/', views.list, name='list'),
+    path('l/<slug:list_id>/u/', views.list_update, name='list_update'),
+    path('t/<slug:task_id>/', views.task, name='task'),
+    path('t/<slug:task_id>/u', views.task_update, name='task_update'),
     path('newlist/', views.new_list, name='newlist'),
     path('newtask/', views.new_task, name='newtask'),
 ]
--- todoshka/doit/views.py
+++ todoshka/doit/views.py
@@ -25,7 +25,7 @@ def index(request):
 @login_required
 def list(request, list_id):
     try:
-        _list = List.objects.select_related().filter(user=request.user).get(id=list_id)
+        _list = List.objects.select_related().filter(user=request.user).get(unique_view_id=list_id)
     except List.DoesNotExist:
         _list = None
     return render(request, 'doit/list.html', {'list': _list})
@@ -34,7 +34,7 @@ def list(request, list_id):
 @login_required
 def list_update(request, list_id):
     try:
-        _list = List.objects.select_related().filter(user=request.user).get(id=list_id)
+        _list = List.objects.select_related().filter(user=request.user).get(unique_view_id=list_id)
 
         if request.method == 'POST':
             if 'true' == request.POST.get('delete'):
@@ -61,14 +61,14 @@ def get_task_util(request, task_id):
     for _list in lists:
         tasks = Task.objects.select_related().filter(to_list=_list)
         for task in tasks:
-            if task.id == task_id:
+            if task.unique_view_id == task_id:
                 is_task_id_valid = True
                 break
         if is_task_id_valid:
             break
 
     if is_task_id_valid:
-        task = Task.objects.get(id=task_id)
+        task = Task.objects.get(unique_view_id=task_id)
     else:
         task = None
 
