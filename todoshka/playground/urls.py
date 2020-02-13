932d91953d2dde4075151b49991405d47c09dbc9
dima <amid.vonavi@gmail.com>
2020-02-14 00:08:27 +0200

    displaying list with CBV

--- todoshka/playground/urls.py
+++ todoshka/playground/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from . import views
 
+
 app_name = 'playground'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('list/', views.ListView.as_view(), name='list'),
+    path('list/<slug:unique_view_id>/', views.ListDetailView.as_view(), name='list_detail'),
 ]
--- todoshka/playground/views.py
+++ todoshka/playground/views.py
@@ -1,5 +1,43 @@
 from django.shortcuts import render, HttpResponse
+from django.views.generic import TemplateView, ListView, DetailView
+
+from doit.models import List, Task
 
 
-def index(request):
-    return render(request, 'playground/playground.html')
+class IndexView(TemplateView):
+    template_name = 'playground/playground.html'
+
+    def get(self, request, *args, **kwargs):
+        req = request.GET
+
+        if request.GET:
+            ret = ""
+            for key in req:
+                ret += "key = {}, val = {}<br>".format(key, request.GET.get(key))
+        else:
+            ret = "None"
+
+        return HttpResponse(ret)
+
+
+class ListView(ListView):
+    template_name = 'playground/list.html'
+    context_object_name = 'list_of_lists'
+
+    def get_queryset(self):
+        return List.objects.select_related().filter(user=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = Task.objects.select_related().filter(user=self.request.user)
+        return context
+
+
+class ListDetailView(DetailView):
+    template_name = 'playground/list_detail.html'
+
+    # def get_queryset(self):
+    #     return List.objects.select_related().filter(user=self.request.user)
+
+    def get_object(self, queryset=None):
+        return List.objects.get(unique_view_id=self.kwargs['unique_view_id'])
